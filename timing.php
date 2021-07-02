a5000f365d1d9f0f796f0150beefbb1fd6112ab3
gawade0710 <86107779+gawade0710@users.noreply.github.com>
2021-07-02 20:26:17 +0530

    Add files via upload

---
+++ timing.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+
+
+</head>
+<body>
+
+
+<div class="col-md-4">
+<br/>
+<div align="center">
+<div id="exam_timer" data-timer="<?php echo $remaining_minutes;?>" 
+style="max-width:400px; width:100%; height:200px;"></div>
+</div>
+</div>
+</body>
+</html>
